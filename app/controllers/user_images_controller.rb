5a9ce087db294a959cbecff70b73fbfdd7c15cb6
hylobates-lar <alison.v.quaglia@gmail.com>
2020-03-15 22:31:01 -0400

    adding coloring page skeleton

--- app/controllers/user_images_controller.rb
+++ app/controllers/user_images_controller.rb
@@ -1,9 +1,15 @@
 class UserImagesController < ApplicationController
-    before_action :authorized, only: [:create]
+    before_action :authorized, only: [:create, :show]
 
     def create
         @user_image = @user.user_images.create(user_image_params)
        
+        render json: @user_image
+    end
+
+    def show 
+        @user_image = @user.user_images.find_by(id: params[:id])
+
         render json: @user_image
     end
 
