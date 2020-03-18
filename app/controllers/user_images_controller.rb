78c0669c2810f79e8dae65c364052c7dd281ad53
hylobates-lar <alison.v.quaglia@gmail.com>
2020-03-17 22:30:24 -0400

    mvp status

--- app/controllers/user_images_controller.rb
+++ app/controllers/user_images_controller.rb
@@ -1,5 +1,11 @@
 class UserImagesController < ApplicationController
-    before_action :authorized, only: [:create, :show]
+    before_action :authorized, only: [:index, :create, :show, :update]
+
+    def index 
+        @user_images = @user.user_images
+
+        render json: @user_images
+    end
 
     def create
         @user_image = @user.user_images.create(user_image_params)
@@ -8,15 +14,21 @@ class UserImagesController < ApplicationController
     end
 
     def show 
-        @user_image = @user.user_images.find_by(id: params[:id])
+        @user_image = @user.user_images.find(params[:id])
 
         render json: @user_image
     end
 
+    def update
+        @user_image = @user.user_images.find(params[:id])
+        @user_image.update!(user_image_params)
+
+        render json: @user_image
+    end
 
     private
 
     def user_image_params
-        params.permit(:user_id, :image_id, :fill_colors)
+        params.permit(:id, :user_id, :image_id, :fill_colors => [])
     end
 end
--- db/migrate/20200313141849_create_user_images.rb
+++ db/migrate/20200313141849_create_user_images.rb
@@ -1,9 +1,10 @@
 class CreateUserImages < ActiveRecord::Migration[6.0]
   def change
+
     create_table :user_images do |t|
       t.integer :user_id
       t.integer :image_id
-      t.string :fill_colors, array: true
+      t.string :fill_colors, array: true, default: Array.new(200).fill("white")
 
       t.timestamps
     end
